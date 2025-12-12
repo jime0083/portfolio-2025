2a95017cc96a8f39bc934c5d91cef61a07a4012a
栗田孟 <akito14140099@gmail.com>
2025-12-12 11:11:05 +0900

    お問い合わせページ修正

--- page-contact.php
+++ page-contact.php
@@ -18,44 +18,12 @@ get_header();
 
     <section class="contact-main">
         <div class="contact-container slide-down-bounce">
-            <form action="#" method="post" class="contact-form">
-                <!-- Row 1 -->
-                <div class="contact-row">
-                    <div class="contact-item">
-                        <label class="text-title">お問い合わせ内容</label>
-                        <input type="text" name="subject" class="contact-input">
-                    </div>
-                    <div class="contact-item">
-                        <label class="text-title">氏名</label>
-                        <input type="text" name="name" class="contact-input">
-                    </div>
-                </div>
-
-                <!-- Row 2 -->
-                <div class="contact-row">
-                    <div class="contact-item">
-                        <label class="text-title">メールアドレス</label>
-                        <input type="email" name="email" class="contact-input">
-                    </div>
-                    <div class="contact-item">
-                        <label class="text-title">電話番号</label>
-                        <input type="tel" name="phone" class="contact-input">
-                    </div>
-                </div>
-
-                <!-- Row 3 -->
-                <div class="contact-row full-width">
-                    <div class="contact-item">
-                        <label class="text-title">お問い合わせ内容詳細</label>
-                        <textarea name="message" class="contact-textarea"></textarea>
-                    </div>
-                </div>
-
-                <!-- Submit Button -->
-                <div class="contact-row full-width">
-                    <button type="submit" class="contact-submit">SEND</button>
-                </div>
-            </form>
+            <?php
+            while (have_posts()) :
+                the_post();
+                the_content();
+            endwhile;
+            ?>
         </div>
     </section>
 </main>
